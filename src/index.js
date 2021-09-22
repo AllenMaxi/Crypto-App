6efdc17a0d13bd7438db819ce1c5c58fedfb42f4
AllenMaxi <maxi_allen97@hotmail.com>
2021-09-22 12:27:32 -0300

    Crypto App

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { Provider } from "react-redux";
+
+import store from "./app/store";
+import "antd/dist/antd.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
