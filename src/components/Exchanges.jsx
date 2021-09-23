2b0ec99471b642b8c5ef21021db8deebd9a23788
AllenMaxi <maxi_allen97@hotmail.com>
2021-09-23 15:10:50 -0300

    exchanges updated

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -10,11 +10,44 @@ const { Text } = Typography;
 const { Panel } = Collapse;
 
 const Exchanges = () => {
-    
+    const {data: exchanges, isFetching} = useGetExchangesQuery()
+    const exchange = exchanges?.data?.exchanges
+    if (isFetching) return <Loader />;
     return (
-        <div>
-            Exchanges
-        </div>
+        <>
+        <Row>
+          <Col span={6}>Exchanges</Col>
+          <Col span={6}>24h Trade Volume</Col>
+          <Col span={6}>Markets</Col>
+          <Col span={6}>Change</Col>
+        </Row>
+        <Row>
+          {exchange.map((exchange) => (
+            <Col span={24}>
+              <Collapse>
+                <Panel
+                  key={exchange.id}
+                  showArrow={false}
+                  header={(
+                    <Row key={exchange.id}>
+                      <Col span={6}>
+                        <Text><strong>{exchange.rank}.</strong></Text>
+                        <Avatar className="exchange-image" src={exchange.iconUrl} />
+                        <Text><strong>{exchange.name}</strong></Text>
+                      </Col>
+                      <Col span={6}>${millify(exchange.volume)}</Col>
+                      <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                      <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                    </Row>
+                    )}
+                >
+                  {HTMLReactParser(exchange.description || '')}
+                </Panel>
+              </Collapse>
+            </Col>
+          ))}
+        </Row>
+      </>
     )
 }
 
